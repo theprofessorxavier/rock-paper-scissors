d82256a9706f34b33dbb8a88a35c5522d751bdc2
Xavier <142161233+theprofessorxavier@users.noreply.github.com>
2025-02-27 22:10:40 +1000

    Add 'getHumanChoice' Function

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 function getComputerChoice(){
-    
+    let random = Math.floor(Math.random()*10);
+    let result = "";
+    if (random<3){
+        result = "rock";
+    } else if (random<6){
+        result = "paper";
+    } else if (random>=6){
+        result = "scissors";
+    }
+    return result;
 }
 
-console.log(Math.random());
-// Create a new function named getComputerChoice.
-// Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
-// Hint: The Math.random method returns a random number that’s greater than or equal to 0 and less than 1. Think about how you can use this to conditionally return one of the multiple choices.
-// Note: Some learners might think that arrays are necessary for this problem, but they aren’t. If you’re unfamiliar with arrays, don’t worry - they’ll be covered later in the curriculum.
+
+function getHumanChoice(){
+    let answer = prompt("Answer: ");
+    let result = "";
+    if (answer.toLowerCase() == "rock"){
+        result = "rock";
+    } else if (answer.toLowerCase() == "paper"){
+        result = "paper";
+    } else if (answer.toLowerCase() == "scissors"){
+        result = "scissors";
+    } else {
+        result = "invalid";
+    }
+    return result;
+}
+
+console.log(getHumanChoice());
+
+
+
+// Step 3: Write the logic to get the human choice
+// Your game will be played by a human player. You will write a function that takes the user choice and returns it.
+
+// Create a new function named getHumanChoice.
+// Write the code so that getHumanChoice will return one of the valid choices depending on what the user inputs.
+// Hint: Use the prompt method to get the user’s input.
+// Test what your function returns by using console.log.
+
+
+
