46dda39dc8776ff2857679a7fcc892aba0a6d06e
Xavier <142161233+theprofessorxavier@users.noreply.github.com>
2025-02-27 22:56:50 +1000

    Add Game Execution Loop

--- script.js
+++ script.js
@@ -67,12 +67,13 @@ function playRound(humanChoice, computerChoice){
 
 }
 
-playRound(getHumanChoice(), getComputerChoice());
-// Your game will be played round by round. You will write a function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
-
-// Create a new function named playRound.
-// Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments.
-// Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
-// Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
-// Increment the humanScore or computerScore variable based on the round winner.
+for(let check = true; check != false;){
+    console.log(`Current Score - Human Score: ${humanScore} Computer Score: ${computerScore}`);
+    playRound(getHumanChoice(), getComputerChoice());
+    let answer = prompt("Do you want to continue? (y/n): ");
+    switch (answer) {
+        case "n": check = false; break;
+        case "y": default: check = true; break;
+    }
+}
 
