45f68dd1dac9680430004588d9fe044ee0d2b29a
Xavier <142161233+theprofessorxavier@users.noreply.github.com>
2025-02-27 21:52:51 +1000

    Add 'getComputerChoice' Function

---
+++ script.js
@@ -0,0 +1,9 @@
+function getComputerChoice(){
+    
+}
+
+console.log(Math.random());
+// Create a new function named getComputerChoice.
+// Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+// Hint: The Math.random method returns a random number that’s greater than or equal to 0 and less than 1. Think about how you can use this to conditionally return one of the multiple choices.
+// Note: Some learners might think that arrays are necessary for this problem, but they aren’t. If you’re unfamiliar with arrays, don’t worry - they’ll be covered later in the curriculum.
