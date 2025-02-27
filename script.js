aa32552625c1127ef37b8d5a12add00be1e2e9d6
Xavier <142161233+theprofessorxavier@users.noreply.github.com>
2025-02-27 22:44:22 +1000

    Add 'playRound' Function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let random = Math.floor(Math.random()*10);
     let result = "";
@@ -10,7 +13,6 @@ function getComputerChoice(){
     }
     return result;
 }
-
 
 function getHumanChoice(){
     let answer = prompt("Answer: ");
@@ -27,17 +29,50 @@ function getHumanChoice(){
     return result;
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice){
+    let result = ""
+    if (humanChoice == "rock") {
+        switch (computerChoice) {
+            case "rock": result = "tie"; break;
+            case "paper": result = "lose"; break;
+            case "scissors": result = "win"; break;
+        }
+    } else if (humanChoice == "paper") {
+        switch (computerChoice) {
+            case "rock": result = "win"; break;
+            case "paper": result = "tie"; break;
+            case "scissors": result = "lose"; break;
+        }
+    } else if (humanChoice == "scissors") {
+        switch (computerChoice) {
+            case "rock": result = "lose"; break;
+            case "paper": result = "win"; break;
+            case "scissors": result = "tie"; break;
+        }
+    } else {
+        result = "invalid";
+    }
 
+    if (result == "win"){
+        console.log(`You win!, ${humanChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}`);
+        ++humanScore;
+    } else if (result == "lose"){
+        console.log(`You lose!, ${computerChoice.toUpperCase()} beats ${humanChoice.toUpperCase()}`);
+        ++computerScore;
+    } else if (result == "tie"){
+        console.log(`It's a tie!, both had ${humanChoice.toUpperCase()}`);
+    } else {
+        console.log("Nothing");
+    }
 
+}
 
-// Step 3: Write the logic to get the human choice
-// Your game will be played by a human player. You will write a function that takes the user choice and returns it.
+playRound(getHumanChoice(), getComputerChoice());
+// Your game will be played round by round. You will write a function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
 
-// Create a new function named getHumanChoice.
-// Write the code so that getHumanChoice will return one of the valid choices depending on what the user inputs.
-// Hint: Use the prompt method to get the user’s input.
-// Test what your function returns by using console.log.
-
-
+// Create a new function named playRound.
+// Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments.
+// Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
+// Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
+// Increment the humanScore or computerScore variable based on the round winner.
 
